f228f76a61f242ec370e0508c541b0035acdd8e3
Shyam Sunder <35172662+shyamgssr@users.noreply.github.com>
2022-11-17 15:39:00 +0530

    Add files via upload

---
+++ Searcharray.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std ;
+
+int main() {
+
+int arr[] = {1,9,4,8,7,5} ;
+int i ;
+int val = 9 ;
+
+for(i= 0 ; i<=5 ; i++)
+{
+
+    if( arr[i] == val)
+    {
+     cout << "Number found at: " << i <<endl ;
+     break ;
+    }
+
+}
+
+if (i == 6) 
+{
+
+cout << "Number not found " << i  ;
+}
+}
