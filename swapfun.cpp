6bf9e2cb73f4d227ec0ebbac168ea2f39491ad74
Shyam Sunder <35172662+shyamgssr@users.noreply.github.com>
2022-11-22 20:09:51 +0530

    Add files via upload

---
+++ swapfun.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std ; 
+
+int main() {
+
+int a = 10 ;
+int b = 5  ;
+
+cout << "Value of a Before Swap " <<a << endl ;
+cout << "Value of b Before Swap " <<b << endl ;
+
+swap(a, b) ;
+
+cout << "Value of a after Swap " <<a << endl ;
+cout << "Value of b after Swap " <<b << endl ;
+
+
+}
